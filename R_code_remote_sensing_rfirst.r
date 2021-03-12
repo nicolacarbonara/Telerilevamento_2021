cb28f1cbbc1709dfea9a31701c6bdcb18d430518
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-03-12 11:19:21 +0100

    Create R_code_remote_sensing_rfirst.r

---
+++ R_code_remote_sensing_rfirst.r
@@ -0,0 +1,7 @@
+# my first code in R for remote sensing
+
+setwd("C:/lab/") #imposto la cartella lab come cartella di lavoro
+library(raster) #carico il pacchetto raster all'interno del quale trovo brick
+p224r63_2011 <- brick("p224r63_2011_masked.grd") #assegno ad una variabile interna un oggetto esterno
+
+plot(p224r63_2011, col=cl) #stampo a video le immagini satellitari
