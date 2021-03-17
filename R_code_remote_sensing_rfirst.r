d199bf727bdce77a97d8939f03213634184415a7
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-03-17 11:25:00 +0100

    Update R_code_remote_sensing_rfirst.r

--- R_code_remote_sensing_rfirst.r
+++ R_code_remote_sensing_rfirst.r
@@ -2,6 +2,7 @@
 
 setwd("C:/lab/") #imposto la cartella lab come cartella di lavoro
 library(raster) #carico il pacchetto raster all'interno del quale trovo brick
-p224r63_2011 <- brick("p224r63_2011_masked.grd") #assegno ad una variabile interna un oggetto esterno
+p224r63_2011 <- brick("p224r63_2011_masked.grd") #assegno ad una variabile interna un oggetto esterno, importo l'immagine satellitare
 
 plot(p224r63_2011, col=cl) #stampo a video le immagini satellitari
+
