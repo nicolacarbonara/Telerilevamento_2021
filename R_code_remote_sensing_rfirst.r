4112da60cb7c8db386868e932a2269bd420c54b4
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-03-24 13:00:03 +0100

    Update R_code_remote_sensing_rfirst.r

--- R_code_remote_sensing_rfirst.r
+++ R_code_remote_sensing_rfirst.r
@@ -11,7 +11,7 @@ cl<-colorRampPalette(c("black","grey","light grey"))(100) #il numero indica quan
 #faccio il plot con il nuovo colore
 plot(p224r63_2011,col=cl)#il primo arg. è l'immagine, il secondo è il colore che prendo dal vettore cl
 #cambiamo colore
-cl<-colorRampPalette(c("black","blue","green","orange","red"))(200)
+cl<-colorRampPalette(c("black","blue","green","orange","red"))(200) #con 200 gradi di variazione di colore
 plot(p224r63_2011,col=cl) #riplotto i nuovi colori
 > p224r63_2011 #il path è il 224 e la riga è la 63
 #class      : RasterBrick 
@@ -23,3 +23,40 @@ plot(p224r63_2011,col=cl) #riplotto i nuovi colori
 #names      :       B1_sre,       B2_sre,       B3_sre,       B4_sre,       B5_sre,        B6_bt,       B7_sre 
 #min values : 0.000000e+00, 0.000000e+00, 0.000000e+00, 1.196277e-02, 4.116526e-03, 2.951000e+02, 0.000000e+00 
 #max values :    0.1249041,    0.2563655,    0.2591587,    0.5592193,    0.4894984,  305.2000000,    0.3692634
+#B1: BLU, B2: VERDE, B3: ROSSO, B4:NIR, B5: MEDIO IR, B6:IR TERMICO, B7: MEDIO IR.
+
+#voglio plottare solo la banda del blu
+dev.off() #clean the current graph
+plot(p224r63_2011$B1_sre) #plotto solo il sottoinsieme della banda blu
+#ora plotto la banda 1 con i colori nuovi definiti
+cl<-colorRampPalette(c("blue","yellow","red","green"))(200)
+plot(p224r63_2011$B1_sre,col=cl) #riplotto i nuovi colori solo per la banda blu
+dev.off()
+#vogliamo un grafico con delle bande specifiche, MultiFrame
+par(mfrow=c(1,2)) #prepara un grafico con 1 riga e 2 colonne
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+par(mfrow=c(2,1)) #prepara un grafico con 2 righe e 1 colonna
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+#con mfrow definisco prima il numero delle righe nel vettore c
+#rifaccio il plot con la nuova gamma di colori
+par(mfrow=c(2,1)) #prepara un grafico con 2 righe e 1 colonna
+plot(p224r63_2011$B1_sre,col=cl) 
+plot(p224r63_2011$B2_sre,col=cl)
+#plot the first four bands of Landsat
+par(mfrow=c(4,1)) #plotto le 4 bande in una sola colonna
+plot(p224r63_2011$B1_sre,col=cl)
+plot(p224r63_2011$B2_sre,col=cl)
+plot(p224r63_2011$B3_sre,col=cl)
+plot(p224r63_2011$B4_sre,col=cl)
+par(mfrow=c(2,2)) #plotto le 4 bande in una 2 colonne e 2 righe
+clb<-colorRampPalette(c("dark blue", "blue", "purple"))(200) #definisco nuovi colori
+plot(p224r63_2011$B1_sre,col=clb)
+clg<-colorRampPalette(c("dark green", "green", "light green"))(200)
+plot(p224r63_2011$B2_sre,col=clg)
+clr<-colorRampPalette(c("dark red", "red", "light pink"))(200)
+plot(p224r63_2011$B3_sre,col=clr)
+clnir<-colorRampPalette(c("red", "orange", "yellow"))(200)
+plot(p224r63_2011$B4_sre,col=clnir)
