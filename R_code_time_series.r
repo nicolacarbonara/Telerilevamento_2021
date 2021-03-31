09998a76dc5980c766604a8dd0b891954ff4b705
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-03-31 13:00:13 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,7 @@
+#Time series analysis
+#Greenland increase of temperature
+#Data and code from Emanuela Cosma
+
+setwd("C:/lab/Greenland") #imposto la cartella greenland come cartella di lavoro
+library(raster) #carico il pacchetto raster all'interno del quale trovo brick
 
