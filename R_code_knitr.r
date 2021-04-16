b25410f451e099144af8971a460e022189e8cb62
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-04-16 11:41:08 +0200

    Create R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,6 +1,9 @@
 # R_code_knitr.r
 setwd("C:/lab/")  #imposto la working directory
-library(knitr) #carico il pacchetto knitr.
+library(knitr) #carico il pacchetto knitr
+#stitch("R_code_Greenland", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #stitch nome del file
+stitch("R_code_Greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
 
 
 
