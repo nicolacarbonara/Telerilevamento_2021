10030cdd235e5416be6c2a55b475e64b2b794e28
nicolacarbonara <80513564+nicolacarbonara@users.noreply.github.com>
2021-05-21 17:19:36 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -1,6 +1,9 @@
 # R_code_variability.r
 library(raster)
 library(RStoolbox)
+library(viridis)
+library(gridExtra)
+library(ggplot2)
 setwd("C:/lab/")
 sent<-brick("sentinel.png")
 #plotto con le bande NIR 1, red 2, green 3, 
@@ -29,11 +32,22 @@ plot(sentpca$map)
 summary(sentpca$model)
 #the first pc explain 67.36804% of the original information
 pc1<-sentpca$map$PC1
-devst5<-focal(pc1,w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+pc1st5<-focal(pc1,w=matrix(1/25, nrow=5, ncol=5), fun=sd)
 clst<-colorRampPalette(c('blue', 'red', 'yellow', 'green', 'black'))(150)
-plot(devst5, col=clst, stretch="lin")
+plot(pc1st5, col=clst, stretch="lin")
 # source function
+source("source_test_lezione.r") #inserisco un pezzo di codice e lo carico direttamente tramite la funzione source, calcola la deviazione standard con matrice 7x7
+source("source_ggplot.r")
+p1<-ggplot()+ #ggplot() apre una finestra in cui contenere un oggetto ggplot
+geom_raster(pc1st5, mapping=aes(x=x, y=y, fill=layer))+
+scale_fill_viridis()#aes=aestetics, riempimento il layer
 
+p2<-ggplot()+ #ggplot() apre una finestra in cui contenere un oggetto ggplot
+geom_raster(pc1st5, mapping=aes(x=x, y=y, fill=layer))+
+scale_fill_viridis(option="magma")
 
-
-
+p3<-ggplot()+ #ggplot() apre una finestra in cui contenere un oggetto ggplot
+geom_raster(pc1st5, mapping=aes(x=x, y=y, fill=layer))+
+scale_fill_viridis(option="turbo")
+#più grafici in una pagina con gridarrange
+grid.arrange(p1,p2,p3, nrow=1)
